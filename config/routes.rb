fceb8f7f44a75ea74a5ff912e093675ec490575f
disha patel <disha.p@hotmail.co.uk>
2016-08-29 18:02:58 +0100

    added new journey form

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
     resources :journeys, only:[:new, :create]
   end
 
-  resources :journey, only:[:index, :show, :edit, :update, :destroy] do
-    resources :passenger, only:[:create]
+  resources :journeys, only:[:index, :show, :edit, :update, :destroy] do
+    resources :passengers, only:[:create]
   end
 
   namespace :user do
