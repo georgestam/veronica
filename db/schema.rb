4f3068a63705a0a7afb9a725bd6d37fba990cf8a
Luke Breen <l.breen1@outlook.com>
2016-08-29 15:15:32 +0100

    Updated to latest DB version with car model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829140832) do
+ActiveRecord::Schema.define(version: 20160829141223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "make"
+    t.string   "model"
+    t.string   "vrn"
+    t.string   "colour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -44,4 +55,5 @@ ActiveRecord::Schema.define(version: 20160829140832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "cars", "users"
 end
