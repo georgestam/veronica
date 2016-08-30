f6c7496d6a54761d6f8e3cac7eff90b479d8814f
Luke Breen <l.breen1@outlook.com>
2016-08-30 14:09:53 +0100

    Added uniqueness for VRN

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,5 +3,5 @@ class Car < ApplicationRecord
   has_many :journeys
 
   validates :make, :name, :colour, presence: true
-  validates :vrn, presence: true, format: { with: /([A-Z]{2}[0-9]{2}[A-Z]{3}$)|([A-Z][0-9]{1,3}[A-Z]{3}$)|([A-Z]{3}[0-9]{1,3}[A-Z]$)|([0-9]{1,4}[A-Z]{1,2}$)|([0-9]{1,3}[A-Z]{1,3}$)|([A-Z]{1,2}[0-9]{1,4}$)|([A-Z]{1,3}[0-9]{1,3}$)/ }
+  validates :vrn, presence: true, uniqueness: true, format: { with: /([A-Z]{2}[0-9]{2}[A-Z]{3}$)|([A-Z][0-9]{1,3}[A-Z]{3}$)|([A-Z]{3}[0-9]{1,3}[A-Z]$)|([0-9]{1,4}[A-Z]{1,2}$)|([0-9]{1,3}[A-Z]{1,3}$)|([A-Z]{1,2}[0-9]{1,4}$)|([A-Z]{1,3}[0-9]{1,3}$)/ }
 end
