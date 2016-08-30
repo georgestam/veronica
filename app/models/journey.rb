2ab5758f7bab2b34b0fbabff73841744af74f8e5
disha patel <disha.p@hotmail.co.uk>
2016-08-30 17:13:00 +0100

    added date can't be in the past validation

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -6,6 +6,13 @@ class Journey < ApplicationRecord
   validates :pick_up_time, presence: true
   validates :pick_up_location, presence: true
   validates :drop_off_location, presence: true
+  validate :journey_date_cannot_be_in_the_past
+
+  def journey_date_cannot_be_in_the_past
+    if pick_up_time < Date.today
+      errors.add(:pick_up_time, "Cannot be in the past")
+    end
+  end
 
 
 end
