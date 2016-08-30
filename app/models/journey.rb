8100a98a068f7a7e1f6d74945c85fa20e7354c73
disha patel <disha.p@hotmail.co.uk>
2016-08-30 10:47:03 +0100

    commiting before testing with seeds

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,4 +1,8 @@
 class Journey < ApplicationRecord
   belongs_to :user
   belongs_to :car
+  validates :seats_available, presence: true, numericality: true, inclusion: {in: (1..7)}
+  validates :pick_up_time, presence: true
+  validates :pick_up_location, presence: true
+  vlaidates :drop_off_location, presence: true
 end
