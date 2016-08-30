7163134799ab73d45f2f2fe6f011fd7d2d80c78a
Luke Breen <l.breen1@outlook.com>
2016-08-30 13:14:53 +0100

    Added validations for car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :journeys
+
+  validates :make, :name, :colour, presence: true
+  validates :vrn, presence: true, format: { with: /([A-Z]{2}[0-9]{2}[A-Z]{3}$)|([A-Z][0-9]{1,3}[A-Z]{3}$)|([A-Z]{3}[0-9]{1,3}[A-Z]$)|([0-9]{1,4}[A-Z]{1,2}$)|([0-9]{1,3}[A-Z]{1,3}$)|([A-Z]{1,2}[0-9]{1,4}$)|([A-Z]{1,3}[0-9]{1,3}$)/ }
 end
