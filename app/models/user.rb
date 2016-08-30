c09e0995cf43e0065a0109f9811bc78c68ba7cab
Luke Breen <l.breen1@outlook.com>
2016-08-30 13:36:19 +0100

    Added email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :passengers
   has_many :cars
   has_many :journeys
+
+  validates :email, uniqueness: true, format: { with: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/ }
 end
