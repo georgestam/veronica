0c910be47484969289509c3155913c55a0071439
Luke Breen <l.breen1@outlook.com>
2016-08-31 18:34:25 +0100

    Added cards for stats, car details and reviews

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+
+  // This will increase the account-progress bar dynamically
+  $('#increase-bar').click(function(){
+    // The variable below is currently hard-coded and needs to be altered later
+    var newprogress = "40";
+
+    $('#account-progress').attr('aria-valuenow', newprogress).css('width',newprogress+'%');
+
+  });
+
+});
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,7 +16,8 @@ class ProfilesController < ApplicationController
 
   def dashboard
     @user = current_user
-
+    @journeys = Journey.where(user: @user)
+    calculate_avg_rating
   end
 
   private
@@ -27,5 +28,17 @@ class ProfilesController < ApplicationController
 
   def user_params
     params.require(:user).permit(:photo, :first_name, :last_name, :email, :phone_number, :description, :gender, :student_id, :date_of_birth, :music_habits, :speaking_habits, :year_of_study, :uni_course, :smoking)
+  end
+
+  def calculate_avg_rating
+    ratings = []
+
+    @journeys.each do |journey|
+      journeys.passengers.each do |passenger|
+        ratings << passenger.driver_rating
+      end
+    end
+
+    @avg_rating = ratings.inject(0){|sum,x| sum + x } / ratings.count if ratings.count != 0
   end
 end
