b54d878be37dff735ebc94f562704c3a7289649b
Luke Breen <l.breen1@outlook.com>
2016-09-02 14:05:44 +0100

    Fixed bugs and created policy scope for journey#index

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,7 +2,7 @@ class JourneysController < ApplicationController
   before_action :set_journey, only:[:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
-    @journeys = Journey.all
+    @journeys = policy_scope(Journey)
   end
 
   def show
--- app/policies/journey_policy.rb
+++ app/policies/journey_policy.rb
@@ -1,7 +1,7 @@
 class JourneyPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
 
