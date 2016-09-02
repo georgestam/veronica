00e15b21370f39694b4b859eedee6e5597b83ea5
Max Philips <maximilien.philips@gmail.com>
2016-09-02 13:06:47 +0100

    facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "users/:id", to: "profiles#show", as: :profile
