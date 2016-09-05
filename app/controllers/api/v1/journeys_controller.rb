f7593de10847174a7b625b2fc253855d41d36719
Luke Breen <l.breen1@outlook.com>
2016-09-05 12:00:04 +0100

    Added journey#index API and journey#update with authentication - but can't check update due to sign in error

--- app/controllers/api/v1/journeys_controller.rb
+++ app/controllers/api/v1/journeys_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::JourneysController < Api::V1::BaseController
-  before_action :set_journey, only: [ :show ]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_journey, only: [ :show, :update]
+
+  def index
+    @journeys = policy_scope(Journey)
+  end
 
   def show
+  end
+
+  def update
+    if @journey.update(journey_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   private
@@ -9,5 +22,19 @@ class Api::V1::JourneysController < Api::V1::BaseController
   def set_journey
     @journey = Journey.find(params[:id])
     authorize @journey  # For Pundit
+  end
+
+  def journey_params
+    params.require(:journey).permit(
+      :seats_available,
+      :pick_up_time,
+      pick_up_location_attributes: [ :address ],
+      drop_off_location_attributes: [ :address ]
+    )
+  end
+
+  def render_error
+    render json: { errors: @journey.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :journeys, only: [ :show]
+      resources :journeys, only: [:index, :show, :update]
     end
   end
 
