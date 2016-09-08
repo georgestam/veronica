60111aaaa4b9b75ebdac12c796c055d1f90adef7
Luke Breen <l.breen1@outlook.com>
2016-09-08 18:49:32 +0100

    Replaced warwick.car.sharing@gmail.com with contact@liftin.co.uk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.liftin.co.uk/" }
+  config.action_mailer.default_url_options = { host: "www.liftin.co.uk" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
