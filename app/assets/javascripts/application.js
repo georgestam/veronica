977676b8833597a6fdce3105951a90f82ca39dce
Jordi Fernandez <jordi@ywarp.com>
2016-09-25 12:07:11 +0100

    setup figaro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+
+
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require jquery.slick
 //= require attachinary
+
 //= require moment
 //= require bootstrap-datetimepicker
 //= require pickers
