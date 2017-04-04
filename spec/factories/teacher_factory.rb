412d9826103b4e960e838f03917209dea9d5673b
Jordi Fernandez <jordi@ywarp.com>
2017-04-04 11:28:25 +0100

    add coverage to .gitignore and gix teacher_factory

--- spec/factories/teacher_factory.rb
+++ spec/factories/teacher_factory.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     bio { Faker::Lorem.paragraph }
     travel_distance { 1 + Random.rand(9) }
     price_hour { 1 + Random.rand(50) }
+    video_URL {"https://www.youtube.com/watch?v=UodA9XdKQkE"}
     
   end
   
