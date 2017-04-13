0cfd4b505725a7fac8d25cfc043bfd04f0883f1f
Jordi Fernandez <jordi@ywarp.com>
2017-04-13 14:51:35 +0100

    remove commented out code used for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   include ApplicationControllerImpl
-  # include SafeLoggerRaygun
 
   #  i18n
   before_action :set_locale
